3c080cc86ac486b20f3c8d07306499aa7dd517c5
vjdavid <vjdavid@gmail.com>
2015-06-22 10:57:14 -0700

    added file __dir__.rb

---
+++ __dir__.rb
@@ -0,0 +1,14 @@
+#Returns the canonicalized absolute path of the directory of the file from which
+#this method is called.
+
+class Holis
+
+ def directory
+   __dir__
+ end
+
+end
+
+a = Holis.new
+a.directory
+

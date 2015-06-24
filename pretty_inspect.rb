12ac1ac82b34d2eb2c0835aa139787a5c117fb24
vjdavid <vjdavid@gmail.com>
2015-06-24 10:35:17 -0700

    added file pretty_inspect.rb

---
+++ pretty_inspect.rb
@@ -0,0 +1,12 @@
+#Inspect and print pretty objects
+
+dog = "Frankie"
+
+numbers = 2
+
+types = ["frenchpudle", "dalmata", "rodweiler", "chiuahua", "grand danes"]
+
+puts types.pretty_inspect
+puts numbers.pretty_inspect
+puts dog.pretty_inspect
+
